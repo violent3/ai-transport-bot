5c81d51ed4c5976306982055a45479cbc1b4d2e4
Igor <142220647+violent3@users.noreply.github.com>
2025-07-18 19:13:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 import os
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+TELEGRAM_TOKEN = os.getenv("7959498262:AAGTeVFOy0ke83n_eSMp6rgk86RPDgqJCCY")
+WEATHER_API_KEY = os.getenv("20dabe77dbf057f0e9656cd31e257cf0")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 CITY_NAME = "Petropavl"
